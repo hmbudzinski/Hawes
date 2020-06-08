c8ae391511f359c7943b15a95db9a003436450f2
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-06-08 15:54:59 -0600

    working on Work page

--- client/src/pages/Work.js
+++ client/src/pages/Work.js
@@ -1,13 +1,48 @@
 import React from "react";
 import MainNav from "../components/Nav";
 import Footer from "../components/Footer"
+import "../styles/main.css"
+import TreeImage from "../styles/tree.jpg"
+// import NerdImage from "../../styles/two.PNG"
+// import WeatherImage from "../../styles/three.PNG"
+// import BudgetImage from "../../styles/four.PNG"
 
 
 function Home() {
     return (
         <div id="homeID">
             <MainNav />
-            Work
+        <hr id="breakline"></hr>
+        <div className="jumbotron">
+        <div className="row" id="workcontainer">
+        <div className="card shadow-drop-2-center">
+            <img src={TreeImage} className="card-img-top workimages" alt="quarantivities"></img>
+            <div className="card-body">
+                <h5 className="card-title">Quarantivities</h5>
+            </div>
+        </div>
+        <div className="card shadow-drop-2-center">
+            <img src={TreeImage} className="card-img-top workimages" alt="Weather"></img>
+            <div className="card-body">
+                <h5 className="card-title">Weather Dashboard</h5>
+            </div>
+        </div>
+
+        <div className="card shadow-drop-2-center">
+            <img src={TreeImage} className="card-img-top workimages" alt="Budget"></img>
+            <div className="card-body">
+                <h5 className="card-title">Budget-Tracker</h5>
+            </div>
+        </div>
+        <div className="card shadow-drop-2-center">
+            <img src={TreeImage} className="card-img-top workimages" alt="Nerdify"></img>
+            <div className="card-body">
+                <h5 className="card-title">Nerdify</h5>
+            </div>
+        </div>
+        </div>
+        </div>
+
             <Footer />    
         </div>
     )
