d16154eb6a444ef1ead47695fa017db21140caf1
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-06-08 16:24:50 -0600

    playing with cards

--- client/src/pages/Work.js
+++ client/src/pages/Work.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MainNav from "../components/Nav";
 import Footer from "../components/Footer"
 import "../styles/main.css"
@@ -7,45 +7,48 @@ import { Card, CardGroup } from "react-bootstrap";
 
 
 function Home() {
+
+    function cardHover(event){
+        // const [isHover, setIsHover] = useState(false);
+        // event.target.style.background = 'red';
+    }
+
     return (
         <div id="homeID">
         <MainNav />
         <CardGroup className="CardGroup">
-            <Card>
-                <Card.Img variant="top" src={TreeImage} />
-                <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                </Card.Text>
-                </Card.Body>
+            <Card
+            // onMouseEnter={() => setIsHover(true)} 
+            // onMouseLeave={() => setIsHover(false)}
+            // onMouseOver={cardHover} 
+            // onMouseLeave={() => setIsHover(false)}
+            >
+                <Card.Img variant="top" src={TreeImage} className="workimage"/>
             </Card>
 
             <Card>
-                <Card.Img variant="top" src={TreeImage} />
-                <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This card has supporting text below as a natural lead-in to additional
-                    content.{' '}
-                </Card.Text>
-                </Card.Body>
+                <Card.Img variant="top" src={TreeImage} className="workimage" />
             </Card>
 
             <Card>
-                <Card.Img variant="top" src={TreeImage} />
-                <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This card has even longer content than the first to
-                    show that equal height action.
-                </Card.Text>
-                </Card.Body>
+                <Card.Img variant="top" src={TreeImage} className="workimage"/>
             </Card>
             </CardGroup>
 
+            
+            <CardGroup className="CardGroup">
+            <Card>
+                <Card.Img variant="top" src={TreeImage} className="workimage"/>
+            </Card>
+
+            <Card>
+                <Card.Img variant="top" src={TreeImage} className="workimage" />
+            </Card>
+
+            <Card>
+                <Card.Img variant="top" src={TreeImage} className="workimage"/>
+            </Card>
+            </CardGroup>
             <Footer />    
         </div>
     )
